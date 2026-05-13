89fd732633362fd5c97fcdb57b60837c150529c6
Ricardo Niederberger Cabral <38954+ricardocabral@users.noreply.github.com>
2026-05-13 14:24:55 -0300

    TP-021 define writable wellness fields

---
+++ internal/tools/update_wellness.go
@@ -0,0 +1,35 @@
+package tools
+
+var updateWellnessSubjectiveScaleFields = []string{
+	"feel",
+	"fatigue",
+	"mood",
+	"sleepQuality",
+	"motivation",
+	"soreness",
+	"stress",
+}
+
+var updateWellnessMeasurementFields = []string{
+	"weight",
+	"bodyFat",
+	"systolic",
+	"diastolic",
+	"bloodGlucose",
+	"lactate",
+	"restingHR",
+	"hrv",
+}
+
+var updateWellnessFreeTextFields = []string{
+	"injury",
+}
+
+var updateWellnessFlagFields = []string{
+	"locked",
+}
+
+var updateWellnessReadOnlyFields = []string{
+	"sleepScore",
+	"_native",
+}
